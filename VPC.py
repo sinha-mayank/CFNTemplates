1447ac18b383bff382beaaf3fcec2ce7e00de8c2
Mayank Sinha <sinha_mayank@hotmail.com>
2016-09-23 11:30:40 +0530

    Added a Public Subnet

--- VPC.py
+++ VPC.py
@@ -18,9 +18,28 @@ parameters = {
 					Description = "Enter the CIDR Range for the VPC to be created",
 					Type = "String",
 					Default = "10.0.0.0/16",
-					AllowedPattern = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$",
+					AllowedPattern = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$"
 
-						)
+						),
+
+	"AvailabilityZone1" : Parameter(
+
+					"AvailabilityZone1",
+					Description = "Select the first AvailabilityZone",
+					Type = "String",
+					Default = "us-west-2a"
+
+									),
+
+	"CIDRPublicSubnet1" : Parameter(
+
+					"CIDRPublicSubnet1",
+					Description = "Enter the CIDR range for PublicSubnet1 in AZ1",
+					Type = "String",
+					Default = "10.0.128.0/20",
+					AllowedPattern = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\\/([0-9]|[1-2][0-9]|3[0-2]))$"
+
+									)
 
 
 			}
@@ -63,7 +82,26 @@ resources = {
 					VpcId = Ref("VPC"),
 					InternetGatewayId = Ref("IGW")
 
-											  )
+											  ),
+
+
+# Create Public Subnet 1
+
+	"PublicSubnet1" : ec2.Subnet(
+
+					"PublicSubnet1",
+					CidrBlock = Ref("CIDRPublicSubnet1"),
+					VpcId = Ref("VPC"),
+					AvailabilityZone = Ref("AvailabilityZone1"),
+					MapPublicIpOnLaunch = "True",
+					Tags = Tags(
+
+								IoCluster = Ref("AWS::StackName"),
+								Name = Join("-",[Ref("AWS::StackName"),"PublicSubnet1"])
+
+								)
+
+								)
 
 			}
 
